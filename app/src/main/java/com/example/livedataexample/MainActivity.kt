ae09d52acf03f82c62a2817006729a0a3951eeed
byTurker <mr_turker@hotmail.com>
2021-08-09 11:01:29 +0300

    Integrate dataBinding and viewModel

--- app/src/main/java/com/example/livedataexample/MainActivity.kt
+++ app/src/main/java/com/example/livedataexample/MainActivity.kt
@@ -3,10 +3,24 @@ package com.example.livedataexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.livedataexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel : MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.countText.text = viewModel.getCurrentCount().toString()
+        binding.button.setOnClickListener {
+            binding.countText.text = viewModel.getUpdatedCount().toString()
+        }
     }
 }
---
+++ app/src/main/java/com/example/livedataexample/MainActivityViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.livedataexample
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    private var count = 0
+
+    fun getCurrentCount(): Int {
+        return count
+    }
+
+    fun getUpdatedCount(): Int {
+        return ++count
+    }
+}
